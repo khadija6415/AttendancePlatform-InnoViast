57e28159bc111d80936fe92125bfe603ea2b7f5c
Khadija <khdjwazeer@gmail.com>
2026-06-26 16:40:28 +0500

    Setup backend with Express and connect MongoDB Atlas

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700">
+        Tailwind is Working! 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
