b10f718a5bbd635da45bc1fdd4b4360b97b8f2d6
Khadija <khdjwazeer@gmail.com>
2026-06-26 17:03:28 +0500

    Add User, Class, and Attendance models

---
+++ server/models/Class.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const classSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  instructor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Class', classSchema);
